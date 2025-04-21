230399fad9f72a35c31db909d5b5e4584432684b
neomdavid <neomanaogdavid@gmail.com>
2025-04-21 20:51:51 +0800

    show selected images

---
+++ .history/src/pages/user/Community_20250421195843.jsx
@@ -0,0 +1,209 @@
+import React, { useState } from "react";
+import { ArrowLeft } from "phosphor-react";
+import profile1 from "../../assets/profile1.png";
+import post1 from "../../assets/post1.jpg";
+import post2 from "../../assets/post2.jpg";
+import post3 from "../../assets/post3.jpg";
+import post4 from "../../assets/post4.jpg";
+import post5 from "../../assets/post5.jpg";
+
+import {
+  PostCard,
+  CustomInput,
+  Heading,
+  FilterButton,
+  AnnouncementCard,
+  CustomSearchBar,
+} from "../../components";
+
+const Community = () => {
+  const [showAside, setShowAside] = useState(false);
+
+  return (
+    <main className="pl-6 text-primary text-lg flex gap-x-6 max-w-[1350px] m-auto relative mt-12">
+      <article className="flex-8 shadow-xl p-12 rounded-lg w-[100vw] lg:w-[65vw]">
+        <CustomSearchBar />
+        <section className="flex gap-x-2 font-semibold w-full mb-8">
+          <FilterButton text="Popular" />
+          <FilterButton text="Latest" />
+          <FilterButton text="My Posts" />
+        </section>
+        <Heading
+          text="Stay /ahead/ of dengue."
+          className="text-[47px] sm:text-7xl lg:text-8xl text-center mb-4 leading-21"
+        />
+        <p className=" text-lg sm:text-xl sm:mt-0 text-center font-semibold text-primary mb-6">
+          Real-Time Dengue Updates from the Community.
+        </p>
+        <section className="bg-base-200 px-8 py-5 rounded-lg mb-4">
+          <p className="font-semibold text-lg text-center mb-3 lg:text-left">
+            Share your experience with the Community
+          </p>
+          <hr className="text-accent mb-4" />
+          <button
+            onClick={() => document.getElementById("my_modal_4").showModal()}
+            className="w-full hover:cursor-pointer"
+          >
+            <CustomInput profileSrc={profile1} showImagePicker={true} />
+          </button>
+        </section>
+        {/* POST MODAL */}
+        <dialog id="my_modal_4" className="modal text-lg text-primary">
+          <div className="modal-box w-10/12 max-w-5xl">
+            <form method="dialog">
+              <button className="btn btn-sm text-3xl font-bold btn-circle btn-ghost absolute right-8 top-8.5">
+                ✕
+              </button>
+            </form>
+            <main className="p-3">
+              <p className="text-4xl font-bold text-center">New Post</p>
+              <hr className="text-gray-300 mt-4 mb-6" />
+              <form className="flex ">
+                <div>
+                  <img
+                    src={profile1}
+                    className="h-15 w-15 rounded-full mr-4"
+                    alt="Profile"
+                  />
+                </div>
+                <div className="flex flex-col w-full gap-4">
+                  <div className="flex gap-2 w-full">
+                    <p className="font-bold mt-1.5">
+                      📍Location: <span className="text-error">*</span>
+                    </p>
+                    <div className="flex-1 flex-col">
+                      <div className="flex gap-2 mb-2">
+                        <input
+                          className="flex-1 bg-base-200 px-5 py-2 rounded-full"
+                          placeHolder="City"
+                        />
+                        <input
+                          className="flex-1 bg-base-200 px-5 py-2 rounded-full"
+                          placeHolder="Barangay"
+                        />
+                      </div>
+                      <input
+                        className="flex-1 bg-base-200 px-5 py-2 rounded-full w-full"
+                        placeHolder="Longitute and Latitude"
+                      />
+                    </div>
+                  </div>
+                  <div className="flex flex-col w-full">
+                    <div className="flex gap-2 items-center w-full">
+                      <p className="font-bold">
+                        🕑Date & Time: <span className="text-error">*</span>
+                      </p>
+                      <div className="flex-1 flex-col">
+                        <div className="flex gap-2">
+                          <input
+                            className="flex-1 bg-base-200 px-5 py-2 rounded-full"
+                            placeHolder="Date"
+                          />
+                          <input
+                            className="flex-1 bg-base-200 px-5 py-2 rounded-full"
+                            placeHolder="Time"
+                          />
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="flex flex-col  w-full">
+                    <div className="flex gap-2 items-center w-full">
+                      <p className="font-bold">
+                        ⚠️Choose Report Type:{" "}
+                        <span className="text-error">*</span>
+                      </p>
+                      <input
+                        className="flex-1 bg-base-200 px-5 py-2 rounded-full"
+                        placeHolder="Choose Report Type"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </form>
+            </main>
+          </div>
+        </dialog>
+        <section className="bg-base-200 px-8 py-6 rounded-lg flex flex-col gap-y-6">
+          <PostCard
+            profileImage={profile1}
+            username="Anonymous Crocodile"
+            timestamp="1 hour ago"
+            location="Barangay Bagumbayan, Taguig City"
+            dateTime="February 19, 2025 – 9:45 AM"
+            reportType="Breeding Site Found"
+            description="Discovered stagnant water in uncovered water containers behind a residential area. Mosquito larvae were visible. Urgent clean-up needed to prevent mosquito breeding."
+            likes="24k"
+            comments="24k"
+            shares="5k"
+          />
+
+          <PostCard
+            profileImage={profile1}
+            username="Anonymous Hippo"
+            timestamp="2 days ago"
+            location="Pasay City Market Area"
+            dateTime="February 17, 2025 – 3:20 PM"
+            reportType="Suspected Dengue Case"
+            description="Vendor reported experiencing high fever and body aches. Advised to seek medical attention. The market area has multiple potential breeding spots"
+            likes="24k"
+            comments="24k"
+            shares="5k"
+            images={[post1, post2, post3, post4, post5]}
+          />
+          <PostCard
+            profileImage={profile1}
+            username="Anonymous Crocodile"
+            timestamp="1 hour ago"
+            location="Barangay Bagumbayan, Taguig City"
+            dateTime="February 19, 2025 – 9:45 AM"
+            reportType="Breeding Site Found"
+            description="Discovered stagnant water in uncovered water containers behind a residential area. Mosquito larvae were visible. Urgent clean-up needed to prevent mosquito breeding."
+            likes="24k"
+            comments="24k"
+            shares="5k"
+          />
+          <PostCard
+            profileImage={profile1}
+            username="Anonymous Crocodile"
+            timestamp="1 hour ago"
+            location="Barangay Bagumbayan, Taguig City"
+            dateTime="February 19, 2025 – 9:45 AM"
+            reportType="Breeding Site Found"
+            description="Discovered stagnant water in uncovered water containers behind a residential area. Mosquito larvae were visible. Urgent clean-up needed to prevent mosquito breeding."
+            likes="24k"
+            comments="24k"
+            shares="5k"
+          />
+        </section>
+      </article>
+
+      <aside
+        className={`bg-base-300 px-6 py-8 shadow-2xl rounded-sm overflow-y-scroll transition-transform duration-300 ease-in-out 
+    fixed inset-y-0 right-0 w-[70vw] top-[68px] max-w-[70vw] pt-20 lg:pt-6 z-50 lg:z-0 lg:sticky lg:top-22 lg:h-[calc(100vh-1.5rem)] 
+    lg:w-[35vw] lg:max-w-[450px] lg:shadow-sm ${
+      showAside ? "translate-x-0" : "translate-x-full"
+    } lg:translate-x-0`}
+      >
+        <AnnouncementCard />
+        <button
+          onClick={() => setShowAside(false)}
+          className="absolute top-4 right-4 lg:hidden bg-primary text-white p-2 rounded-full hover:cursor-pointer hover:bg-white hover:text-primary transition-all duration-200"
+        >
+          <ArrowLeft size={18} className="rotate-180" />
+        </button>
+      </aside>
+
+      {!showAside && (
+        <button
+          onClick={() => setShowAside(true)}
+          className="fixed bottom-[40vh] right-[-1px] z-50 lg:hidden bg-primary text-white p-2 py-4 shadow-xl rounded-sm hover:cursor-pointer hover:bg-white hover:text-primary transition-all duration-200"
+        >
+          <ArrowLeft size={20} />
+        </button>
+      )}
+    </main>
+  );
+};
+
+export default Community;
---
+++ .history/src/pages/user/Login_20250421184424.jsx
@@ -0,0 +1,143 @@
+import { CustomFormInput, LogoNamed } from "../../components";
+import { Link, useNavigate } from "react-router-dom";
+import manHighHand from "../../assets/man_highhand.png";
+import { useState } from "react";
+import { useLoginMutation } from "../../api/dengueApi";
+import { toast } from "react-toastify";
+import { useSelector, useDispatch } from "react-redux";
+import { setCredentials } from "../../features/authSlice";
+
+const Login = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  console.log(email);
+
+  const [login, { isLoading, isError, error }] = useLoginMutation("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await login({ email, password }).unwrap();
+      console.log(response);
+      const { user, token: accessToken } = response;
+      dispatch(setCredentials({ user, token }));
+      toast.success("Logged in successfully!", {
+        position: "top-center", // where the toast shows
+        autoClose: 3000, // closes after 3 seconds
+        hideProgressBar: false, // shows progress bar
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        className: "text-[14px]", // custom styling via Tailwind
+      });
+      navigate("/mapping");
+    } catch (err) {
+      console.error("Login failed:", err);
+    }
+  };
+
+  return (
+    <main className="flex justify-center items-center relative h-[100vh] overflow-hidden">
+      <div className="absolute left-16 lg:left-20 xl:left-22 top-10">
+        <LogoNamed
+          textSize="text-[28px] lg:text-5xl xl:text-5xl 2xl:text-5xl"
+          iconSize="h-11 w-11 lg:h-16 lg:w-16 xl:h-16 xl:w-16 2xl:h-16 2xl:w-16"
+        />
+      </div>
+      <div className="hidden absolute lg:block z-[-1000] text-primary bg-primary w-10 top-0 bottom-0 left-0 xl:w-12.5 2xl:w-13">
+        h
+      </div>
+      <img
+        src={manHighHand}
+        className="z-10000 absolute hidden left-[-2px] bottom-[-25px] w-203 lg:block xl:w-250 xl:right-249 2xl:w-260 "
+      />
+
+      <section
+        className="w-[87vw] h-[80vh] max-w-220 mt-25 rounded-2xl shadow-[4px_4px_8px_rgba(0,0,0,0.40)]  text-primary  bg-white py-12 px-[7%] lg:px-25 flex flex-col justify-center items-center text-center text-lg lg:text-xl
+      lg:shadow-none lg:max-w-none lg:m-0 lg:rounded-none lg:absolute lg:right-0 lg:top-0  lg:h-[100vh] lg:w-[60vw] xl:w-250  "
+      >
+        <h1 className="mb-4 text-7xl lg:text-8xl ">Welcome back!</h1>
+        <p className="mb-10">
+          <span className="font-bold">Login</span> to stay informed and help
+          prevent dengue outbreaks in your community.
+        </p>
+        <form
+          onSubmit={handleSubmit}
+          className="flex flex-col items-center gap-y-6 lg:gap-y-8 w-[85%]"
+        >
+          <CustomFormInput
+            label="Email"
+            type="email"
+            theme="light"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+
+          <CustomFormInput
+            label="Password"
+            type="password"
+            theme="light"
+            isConfirm={true}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+
+          <div className="mt-[-1px] mb-4 flex  w-full justify-between">
+            <div className="flex justify-center  items-center gap-x-2">
+              <input
+                type="checkbox"
+                defaultChecked
+                className="checkbox checkbox-lg border-primary bg-transparent checked:bg-transparent checked:text-primary checked:border-primary "
+              />
+              <label className="text-md lg:text-[14px]">Remember Me</label>
+            </div>
+            <p className="font-semibold text-[14px] italic hover:underline hover:cursor-pointer">
+              Forgot password?
+            </p>
+          </div>
+          <div className="flex flex-col h-full"></div>
+
+          <button
+            className={`bg-primary  font-extrabold shadow-[2px_6px_3px_rgba(0,0,0,0.20)] font-bold text-white w-xs py-3 px-4 rounded-2xl hover:cursor-pointer hover:bg-base-200/60 transition-all duration-300 ${
+              isLoading && "bg-gray-100 disabled"
+            }`}
+          >
+            Login
+          </button>
+          {isError && (
+            <p className="mt-[-10px] font-semibold text-red-500 font-light italic text-md">
+              {error?.data?.message ||
+                "Login failed. Please check your credentials."}
+            </p>
+          )}
+          <div className="flex w-[60%] gap-x-4 mb-[-8px] ">
+            <div className="flex-1 border-t-1 border-primary/60 mt-3 text-white  ">
+              -
+            </div>
+            <div className="text:sm lg:text-[13px] mb-6">or Login With</div>
+            <div className="flex-1 border-t-1 border-primary/60 mt-3 text-white  ">
+              -
+            </div>
+          </div>
+          <button className="bg-primary mb-2 font-extrabold shadow-[2px_6px_3px_rgba(0,0,0,0.20)] font-bold text-white w-xs py-3 px-4 rounded-2xl hover:cursor-pointer hover:bg-base-200/60 transition-all duration-300">
+            Google
+          </button>
+        </form>
+        <p className="mt-6 text-md lg:text-[14px]">
+          Don't have an account?{" "}
+          <Link
+            to="/signup"
+            className="font-bold hover:underline hover:cursor-pointer"
+          >
+            Sign Up
+          </Link>
+        </p>
+      </section>
+    </main>
+  );
+};
+
+export default Login;
